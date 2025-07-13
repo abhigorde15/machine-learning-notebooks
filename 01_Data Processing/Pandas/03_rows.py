7b58090da5de8e761c93b6ddac6e040f436a6ef9
abhigorde15 <166307032+abhigorde15@users.noreply.github.com>
2025-07-13 18:00:49 +0530

    Refactor folder structure: renamed all modules with numeric prefixes (e.g., 01_datapreprocessing) for better organization

--- 01_Data Processing/Pandas/03_rows.py
+++ 01_Data Processing/Pandas/03_rows.py
@@ -1,10 +1,11 @@
 #head(n) and tail(n)
 import pandas as pd
-df = pd.read_csv("Pandas/sales_data_sample.csv", encoding="latin1")
-df.head(6)
-print(df)#display starting 6 
-print("Displaying Information of df")
-print(df.info())
+df = pd.read_csv(r"D:\AIML\AIML\01_Data Processing\Pandas\sales_data_sample.csv", encoding="latin1")
+# df.head(6)#display starting 6 
+# print(df)
+
+# print("Information of data:")
+# df.info()# class,entries,column ,nonnull count,dtype, xx rows * yy cols
 
 data ={
   "Name" : ["Ram","Shyam","Ghanshyam","Jagdish"],
@@ -14,6 +15,6 @@ data ={
 }
 df = pd.DataFrame(data)
 print("Sample Data ")
-print(df)
+#print(df)
 print("\n Descriptive Statistic")
 print(df.describe())
